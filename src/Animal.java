b2cdddcefba6282ac15d2c8d9dc1816e0f394284
Marti <Martinnh1993@gmail.com>
2021-09-21 14:17:59 +0200

    with if statement in speak

--- src/Animal.java
+++ src/Animal.java
@@ -1,55 +1,62 @@
 public class Animal {
     //declaring the variables I need
-    private String animal;
     private int xCoordinate;
     private int yCoordinate;
 
     //making a constructor
-    public Animal (String animal, int xInitial, int yInitial) {
-        this.animal = animal;
+    public Animal(int xInitial, int yInitial) {
         this.xCoordinate = xInitial;
         this.yCoordinate = yInitial;
     }
+
     //making private setters for the x and y so i can only change the x and y coordinates via the move method
-    private void setXCoordinate(int xCoordinate) {this.xCoordinate = xCoordinate;}
-    private void setYCoordinate(int yCoordinate) {this.yCoordinate = yCoordinate;}
+    private void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    private void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 
     //making getters for all the variables
-    public int getXCoordinate() {return xCoordinate;}
-    public int getYCoordinate() {return yCoordinate;}
-    public String getAnimal() {return animal;}
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
 
     //making a move method for the rabbit and snake with if statement, so it can't move outside the perimeters of the board
     public void move(int xChange, int yChange) {
         //establishing the borders
-        if ((xChange+this.xCoordinate)<=10 && (xChange+xCoordinate)>=1 && yChange+this.yCoordinate<=10 && (yChange+yCoordinate)>=1) {
-            if (xChange<=1 && xChange>=-1 && yChange<=1 && yChange>=-1 && xChange != 0 || yChange != 0) {
+        if ((xChange + this.xCoordinate) <= 10 && (xChange + xCoordinate) >= 1 && yChange + this.yCoordinate <= 10 && (yChange + yCoordinate) >= 1) {
+            if (xChange <= 1 && xChange >= -1 && yChange <= 1 && yChange >= -1 && xChange != 0 || yChange != 0) {
                 setXCoordinate(this.xCoordinate + xChange);
                 setYCoordinate(this.yCoordinate + yChange);
-            }
-            else {
+            } else {
                 System.out.println("invalid move");
             }
-        }
-        else {
+        } else {
             System.out.println("Invalid move");
         }
     }
 
     public void Speak(String Keyword) {
-        if(Keyword.equalsIgnoreCase("initial")) {
-            System.out.println("I am the " + this.getAnimal() + " my initial position is: " + this.getXCoordinate() + "," + this.getYCoordinate());
-        }
-        else if(Keyword.equalsIgnoreCase("position")) {
-        System.out.println("I am the " + this.getAnimal() + ", I am standing on " + this.getXCoordinate() + "," + this.getYCoordinate());
-        }
-        else if(Keyword.equalsIgnoreCase("please")) {
-            System.out.println("Noooooo Please don't eat me!!");
-        }
-        else if (Keyword.equalsIgnoreCase("eat")) {
-            System.out.println("Ahhhrrrr im eating you haha!");
-        }
 
-    }
+        if (Keyword.equalsIgnoreCase("RabbitPosition")) {
+            System.out.println("I am the Rabbit, I am standing on " + this.getXCoordinate() + "," + this.getYCoordinate());
+            }
+            if (Keyword.equalsIgnoreCase("SnakePosition")) {
+                System.out.println("I am the Snake, I am standing on " + this.getXCoordinate() + "," + this.getYCoordinate());
+            }
+            else if (Keyword.equalsIgnoreCase("RabbitPlease")) {
+                System.out.println("Noooooo Please don't eat me!!");
+            }
+            else if (Keyword.equalsIgnoreCase("SnakeEat")) {
+                System.out.println("Ahhhrrrr im eating you haha!");
+            }
+
+        }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        //initializing the snake and rabbit with start coordinates
-        Animal rabbit = new Animal("Rabbit",1,1);
-        Animal snake = new Animal("Snake",10, 10);
+            //initializing the snake and rabbit with start coordinates
+        Animal rabbit = new Animal(1,1);
+        Animal snake = new Animal(10, 10);
 
-        //printing the initial position
-        rabbit.Speak("initial");
-        snake.Speak("initial");
+            //printing the initial position
+        rabbit.Speak("RabbitPosition");
+        snake.Speak("SnakePosition");
 
-        //the state of the rabbit
+            //the state of the rabbit
         boolean alive=true;
         while (alive) {
-            //if the snake and rabbit within 1 square of each other, only the snake is going to move
+                    //if the snake and rabbit within 1 square of each other, only the snake is going to move
             if (Math.abs(snake.getXCoordinate() - rabbit.getXCoordinate())==1 && Math.abs(snake.getYCoordinate() - rabbit.getYCoordinate())==1) {
-                rabbit.Speak("please");
+                rabbit.Speak("RabbitPlease");
                 snake.move(-1, -1);
-                snake.Speak("position");
-                snake.Speak("eat");
+                snake.Speak("SnakePosition");
+                snake.Speak("SnakeEat");
                 alive=false;
             }
             else {
-                //the snake and rabbit is moving and saying their positions
+                    //the snake and rabbit is moving and saying their positions
                 rabbit.move(1, 0);
-                rabbit.Speak("position");
+                rabbit.Speak("RabbitPosition");
                 snake.move(0, -1);
-                snake.Speak("position");
+                snake.Speak("SnakePosition");
             }
         }
     }
